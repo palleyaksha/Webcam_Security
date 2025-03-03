74cbe74eeb0c2ac6268c4c5ce0dc8c06d9f2633e
palleyaksha <124580642+palleyaksha@users.noreply.github.com>
2025-03-03 23:18:11 +0530

    Add files via upload

---
+++ maze.py
@@ -0,0 +1,92 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+ROWS = 50
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Maze Solver")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLUE
+
+    def is_start(self):
+        return self.color == YELLOW
+
+    def is_end(self):
+        return self.color == WHITE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col - 1])
+
+def draw_grid(win, rows, width):
+    for i in range(rows):
+        pygame.draw.line(win, GREEN, (0, i * width), (rows * width, i * width))
+        pygame.draw.line(win, GREEN, (i * width, 0), (i * width, rows * width))
+
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+def get_clicked_pos(pos, rows, width):
+    y, x = pos
+    row = y // width
+    col = x // width
+    return row, col
+
+def heuristic(a, b):
+    return abs(a.row - b.row) + abs(a.col - b.col)
+
+def astar_algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 
